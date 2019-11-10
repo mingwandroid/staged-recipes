0421b23e03e739baa923b1db19dffd7b43679e72
Ray Donnelly <mingw.android@gmail.com>
2019-11-10 23:04:21 +0100

    Some progress on MoltenVK

--- recipes/moltenvk/build.sh
+++ recipes/moltenvk/build.sh
@@ -1,7 +1,17 @@
-bash -x ./fetchDependencies -v
+SKIP_XCODEBUILD=yes  \
+  bash -x ./fetchDependencies -v
+export SDKROOT=${CONDA_BUILD_SYSROOT}
+cmake -DCMAKE_CXX=${CXX}  \
+      -DCMAKE_OSX_SYSROOT=${CONDA_BUILD_SYSROOT}  \
+      --debug-trycompile -Wdev --debug-output --trace  \
+      ExternalDependencies.CMakeList.txt | tee ExternalDependencies.config.log
+make -j${CPU_COUNT} all | tee ExternalDependencies.make.log
 pushd MoltenVK
-  cmake -DCMAKE_CXX=${CXX} .
-  make -j${CPU_COUNT}
+  cmake -DCMAKE_CXX=${CXX}  \
+        -DCMAKE_OSX_SYSROOT=${CONDA_BUILD_SYSROOT}  \
+        --debug-trycompile -Wdev --debug-output --trace  \
+        . | tee MoltenVK.config.log
+  make -j${CPU_COUNT} | tee MoltenVK.make.log
 popd
 # make macos
 # cmake .
